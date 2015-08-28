33615e348a51dbb800b5f5240dd6207fd0710d50
mcorbin <corbin.math@gmail.com>
2015-08-28 15:29:27 +0200

    rainbow delimiters

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "violet red"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "gold"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "royal blue"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "burlywood"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "chartreuse4")))))
