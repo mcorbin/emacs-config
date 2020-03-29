3b4f7d1c8dc78a6801b76de8922553fb595a0743
mcorbin <corbin.math@gmail.com>
2020-03-29 20:03:14 +0200

    cleanup

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flycheck-clj-kondo yaml-mode vagrant-tramp vagrant use-package tuareg toml-mode terraform-mode rainbow-delimiters racer puppet-mode popwin persp-projectile paredit nyan-mode neotree multiple-cursors move-text markdown-mode+ manage-minor-mode magit-svn magit-annex lua-mode highlight-parentheses helm-swoop helm-projectile helm-make helm-gtags helm-google helm-descbinds helm-circe guru-mode groovy-mode gotest golint gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-annex geiser flycheck-rust expand-region engine-mode elpy dockerfile-mode discover-my-major company-jedi company-go clojure-mode-extra-font-locking cider cargo adoc-mode ace-window ace-jump-mode))))
+    (puppet-mode terraform-mode groovy-mode golint gotest company-go go-mode flycheck-rust cargo racer rust-mode company-jedi jedi-core elpy vagrant-tramp vagrant dockerfile-mode toml-mode yaml-mode markdown-mode+ markdown-mode adoc-mode nyan-mode cider clojure-mode flycheck-clj-kondo highlight-parentheses paredit rainbow-delimiters lua-mode guru-mode helm-projectile projectile multiple-cursors git-timemachine gitattributes-mode gitconfig-mode gitignore-mode company helm-swoop helm-make move-text helm-gtags helm-descbinds helm manage-minor-mode discover-my-major flycheck popwin async ace-jump-mode expand-region use-package magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,13 +1,3 @@
-;; Initial emacs configuration files. We'll keep it as small as possible
-;; as the real configuration appends in an org file (litterate config.)
-
-
-;; Support for Emacs 24 and higher only
-(let ((minver 24))
-  (unless (>= emacs-major-version minver)
-    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-
-
 ;; Bootstrap 'use-package
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -20,11 +10,6 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-
-;; Keep track of loading time
-(defconst emacs-start-time (current-time))
-
-
 (package-initialize nil)
 ;; Add custom lisp files to the load-path
 (add-to-list 'load-path "~/.emacs.d/lisp")
@@ -34,21 +19,8 @@
 ;; initialize all ELPA packages
 (require 'setup-package)
 
-;; (setq package-enable-at-startup nil)
-(let ((elapsed (float-time (time-subtract (current-time)
-					   emacs-start-time))))
-  (message "Loaded packages in %.3fs" elapsed))
-
 ;; Make sure we have a decent and recent org-mode version
 (require 'org)
-(when (string-match "^[1234567]" (org-version))
-  (progn
-    (warn "Org-mode is out of date. We expect org 8 or higher, but instead we have %s" (org-version))
-    (warn "Force the installation from org elpa.")
-    (package-install 'org)
-    (unload-org-mode)
-    (require 'org)
-    ))
 
 ;; keep customize settings in their own file
 (setq custom-file
@@ -62,7 +34,4 @@
 
 (org-babel-load-file "~/.emacs.d/emacs.org")
 
-(let ((elapsed (float-time (time-subtract (current-time)
-					   emacs-start-time))))
-  (message "Loaded settings...done in %.3fs" elapsed))
 
