704859ed29fb1c15e9aad225543fbd70d9f011d4
mcorbin <corbin.math@gmail.com>
2020-03-31 22:10:26 +0200

    rainbow delimiters colors

--- custom.el
+++ custom.el
@@ -11,4 +11,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "white smoke"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "magenta1"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "gold"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "MediumPurple2"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "burlywood"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "aquamarine1"))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "red1")))))
