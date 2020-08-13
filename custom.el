be5b5db286d138b81e4e58ed76c11579689e4bb5
mcorbin <corbin.math@gmail.com>
2020-08-13 12:58:19 +0200

    fix

--- custom.el
+++ custom.el
@@ -11,7 +11,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
  '(rainbow-delimiters-depth-1-face ((t (:foreground "white smoke"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "deep sky blue"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "dark orange"))))
