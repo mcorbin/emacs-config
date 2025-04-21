19f41c09a972bcca67745e759960366a131e1056
mcorbin <corbin.math@gmail.com>
2025-04-21 11:11:56 +0200

    MaizAI configuration

--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-ui lsp-mode flycheck-golangci-lint php-mode zig-mode marginalia selectrum-prescient embark-consult puppet-mode terraform-mode groovy-mode golint gotest company-go go-mode flycheck-rust cargo racer rust-mode company-jedi jedi-core elpy vagrant-tramp vagrant dockerfile-mode toml-mode yaml-mode markdown-mode+ markdown-mode adoc-mode nyan-mode cider clojure-mode flycheck-clj-kondo highlight-parentheses paredit rainbow-delimiters lua-mode guru-mode helm-projectile projectile multiple-cursors git-timemachine gitattributes-mode gitconfig-mode gitignore-mode company helm-swoop helm-make move-text helm-gtags helm-descbinds helm manage-minor-mode discover-my-major flycheck popwin async ace-jump-mode expand-region use-package magit)))
+   '(flycheck-gometalinter editorconfig vue-mode nix-mode lsp-ui lsp-mode flycheck-golangci-lint php-mode zig-mode marginalia selectrum-prescient embark-consult puppet-mode terraform-mode groovy-mode golint gotest company-go go-mode flycheck-rust cargo racer rust-mode company-jedi jedi-core elpy vagrant-tramp vagrant dockerfile-mode toml-mode yaml-mode markdown-mode+ markdown-mode adoc-mode nyan-mode cider clojure-mode flycheck-clj-kondo highlight-parentheses paredit rainbow-delimiters lua-mode guru-mode helm-projectile projectile multiple-cursors git-timemachine gitattributes-mode gitconfig-mode gitignore-mode company helm-swoop helm-make move-text helm-gtags helm-descbinds helm manage-minor-mode discover-my-major flycheck popwin async ace-jump-mode expand-region use-package magit))
+ '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
