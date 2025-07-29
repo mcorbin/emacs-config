6ada3445b3403d5f4e6b2db0558792accdde1de0
Mathieu Corbin <mathieu.corbin@qonto.com>
2025-07-29 17:50:38 +0200

    better python integration

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("46fd293ff6e2f6b74a5edf1063c32f2a758ec24a5f63d13b07a20255c074d399" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" default))
  '(package-selected-packages
    '(flycheck-gometalinter editorconfig vue-mode nix-mode lsp-ui lsp-mode flycheck-golangci-lint php-mode zig-mode marginalia selectrum-prescient embark-consult puppet-mode terraform-mode groovy-mode golint gotest company-go go-mode flycheck-rust cargo racer rust-mode company-jedi jedi-core elpy vagrant-tramp vagrant dockerfile-mode toml-mode yaml-mode markdown-mode+ markdown-mode adoc-mode nyan-mode cider clojure-mode flycheck-clj-kondo highlight-parentheses paredit rainbow-delimiters lua-mode guru-mode helm-projectile projectile multiple-cursors git-timemachine gitattributes-mode gitconfig-mode gitignore-mode company helm-swoop helm-make move-text helm-gtags helm-descbinds helm manage-minor-mode discover-my-major flycheck popwin async ace-jump-mode expand-region use-package magit))
  '(warning-suppress-types '((comp))))
